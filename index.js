902342fba54066c9885ae59d115b91192c3adf93
UnFuzed <Unfuzedyt@gmail.com>
2025-08-02 22:55:56 -0600

    add: basic bot function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+import 'dotenv/config';
+import tmi from 'tmi.js';
+
+const config = {
+    identity: {
+        username: process.env.TWITCH_BOT_USERNAME,
+        password: process.env.TWITCH_OAUTH_TOKEN
+    },
+    channels: [process.env.TWITCH_CHANNEL]
+}
+
+const client = new tmi.Client(config);
+
+client.on('message', (channel, tags, message, self) => {
+
+    console.log(`${tags['display-name']}: ${message}`);
+});
+
+client.connect().catch(console.error);
