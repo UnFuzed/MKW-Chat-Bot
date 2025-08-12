247fda3c36ee57e18e10aa150ff1e53053ea2e94
UnFuzed <Unfuzedyt@gmail.com>
2025-08-12 01:28:56 -0600

    add: increment counter

--- botActions.js
+++ botActions.js
@@ -4,10 +4,31 @@ import OBSWebSocket from 'obs-websocket-js';
  * @param {import('tmi.js').Client} client
  * @param {import('obs-websocket-js').OBSWebSocket} obs
  */
-export default function botActions(client, obs) {
+export default async function botActions(client, obs) {
    
     obs.on('CurrentProgramSceneChanged', (data) => {
         // console.log('Scene changed in OBS!');
         client.say('#UnFuzed_', `Scene changed to ${data.sceneName}`);
     });   
+
+    client.on('message', async (channel, tags, message, self) => {
+        if (self) return; 
+
+        if (message === '!plusone')
+        {
+
+            const { inputSettings } = await obs.call("GetInputSettings", {inputName: 'Counter'})
+
+            let counterValue = parseInt(inputSettings.text) || 0;
+
+            counterValue += 1;
+
+            await obs.call('SetInputSettings', {
+               inputName: 'Counter',  
+                inputSettings: {
+                    text: counterValue.toString()
+                }
+            });
+        }
+    })
 }
